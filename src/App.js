b21a14756f9bda6ea723d3de2720456e7570c98d
53jk1 <bonczalke@gmail.com>
2021-09-02 22:46:36 +0200

    feat: init

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p></p>
+        <h1 itemprop="name">Kacper "53jk1" Bąk</h1>
+        <h2 itemprop="description">Software Engineer &amp; Backend Developer</h2>
+        <h3>I do it cuz I love it.</h3>
+      </header>
+    </div>
+  );
+}
+
+export default App;
